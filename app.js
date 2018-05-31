c7bb12ccd16ef13fa40de1dfe015dbd6bcf8045f
Kevin Ho <kevinho479@gmail.com>
2018-05-30 21:59:13 -0400

    roll two 6 in a roll and lose all score

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ GAME RULES:
 - In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- A player loses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn.
 - The first player to reach 100 points on GLOBAL score wins the game
 
-1. A player loses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good opportunity to use Google to figure this out)
-3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
-
 */
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, previousRoll;
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
   if(gamePlaying){
     var dice = Math.floor(Math.random() * 6) + 1;
+
+    if(previousRoll === 6 && dice === 6){
+      scores[activePlayer] = 0;
+      document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+      nextPlayer();
+    }
+
+    previousRoll = dice;
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'img/dice-' + dice + '.png';
@@ -67,6 +72,7 @@ function init(){
   roundScore = 0;
   activePlayer = 0;
   gamePlaying = true;
+  previousRoll = 0;
 
   document.querySelector('.dice').style.display = 'none';
 
